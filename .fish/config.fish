e39e59eca7007a340f779b18e6482dfa287e9979
Hideaki Nishioka <hideaki.nishioka@gmail.com>
2018-09-19 07:04:29 +0900

    Added golang path and en_US.UTF-8 for config

--- .fish/config.fish
+++ .fish/config.fish
@@ -2,3 +2,7 @@ status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (rbenv init -|psub)
 
 set -x XDG_CONFIG_HOME ~/.config/
+set -x LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
+set -x GOPATH $HOME/go
+set PATH $GOPATH/bin $PATH
