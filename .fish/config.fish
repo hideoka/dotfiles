f7a6fcd39ddc8462e5bb3e78935244b219b5e37c
Hideaki Nishioka <hideaki.nishioka@gmail.com>
2018-09-30 16:32:48 +0900

    Modified fish config

--- .fish/config.fish
+++ .fish/config.fish
@@ -6,3 +6,26 @@ set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 set -x GOPATH $HOME/go
 set PATH $GOPATH/bin $PATH
+
+
+function fco -d "Fuzzy-find and checkout a branch"
+  git branch --all | grep -v HEAD | string trim | fzf | read -l result; and git checkout "$result"
+end
+bind \cc 'fco'
+
+
+function fzf-cdhist-widget -d 'cd to one of the previously visited locations'
+  # Clear non-existent folders from cdhist.
+  set -l buf
+  for i in (seq 1 (count $dirprev))
+    set -l dir $dirprev[$i]
+    if test -d $dir
+      set buf $buf $dir
+    end
+  end
+  set dirprev $buf
+  string join \n $dirprev | tail -r | sed 1d | eval (__fzfcmd) +m --tiebreak=index --toggle-sort=ctrl-r $FZF_CDHIST_OPTS | read -l result
+  [ "$result" ]; and cd $result
+  commandline -f repaint
+end
+bind \cv 'fzf-cdhist-widget'
