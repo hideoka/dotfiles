e47b6b134b104314d9279587d914d9c6d2d83c26
Hideaki Nishioka <hideaki.nishioka@gmail.com>
2018-10-14 11:16:37 +0900

    Added fzf function for branch

--- .zsh/fzf.zsh
+++ .zsh/fzf.zsh
@@ -1,2 +1,9 @@
 export FZF_DEFAULT_COMMAND='rg --files --glob "!.git/*"'
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
+
+git_checkout() {
+  local branches branch
+  branches=$(git branch -vv) &&
+  branch=$(echo "$branches" | fzf +m) &&
+  git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
+}
