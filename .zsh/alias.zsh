170035363aaa5c685ff3cc6d3f593688f39209ff
Hideaki Nishioka <hideaki@hideoka.dev>
2018-10-15 23:17:56 +0900

    Added alias for git commant'

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -2,4 +2,9 @@ alias ls="ls -al"
 
 alias gd="git diff"
 alias gs="git status"
-alias gc="git_checkout"
+alias gck="git_checkout"
+alias ga="git add"
+alias gc="git commit"
+
+alias dcu="docker-compose up"
+alias dcd="docker-compose down"
