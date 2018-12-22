73d486e86a46467008b026ff0be17c755b1b85b8
Hideaki Nishioka <hideaki.nishioka@gmail.com>
2018-12-22 21:56:35 +0900

    Added makefile and change directory

--- etc/deploy.sh
+++ etc/deploy.sh
@@ -10,9 +10,9 @@ do
   [[ "$f" == ".DS_Store" ]] && continue
   [[ "$f" == ".zsh" ]] && continue
 
-  if [ "$f" == ".vim" ]; then
+  if [[ "$f" == ".vim" ]]; then
     ln -snf ~/dotfiles/$f/nvim ~/.config/nvim
-  elif [ "$f" == ".alacritty.yml" ]; then
+  elif [[ "$f" == ".alacritty.yml" ]]; then
     ln -snf ~/dotfiles/$f ~/.config/alacritty/alacritty.yml
   else
     ln -snf ~/dotfiles/$f ~/$f
