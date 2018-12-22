8345106fea6120286076dd8689757a2ba96838e9
Hideaki Nishioka <hideaki@hideoka.dev>
2018-12-22 18:33:01 +0900

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -Ceuo pipefail
+
+export LC_ALL=C
+export LANG=C
+
+for f in .??*
+do
+  [[ "$f" == ".git" ]] && continue
+  [[ "$f" == ".DS_Store" ]] && continue
+  [[ "$f" == ".zsh" ]] && continue
+
+  if [ "$f" == ".vim" ]; then
+    ln -snf ~/dotfiles/$f/nvim ~/.config/nvim
+  elif [ "$f" == ".alacritty.yml" ]; then
+    ln -snf ~/dotfiles/$f ~/.config/alacritty/alacritty.yml
+  else
+    ln -snf ~/dotfiles/$f ~/$f
+  fi
+done
