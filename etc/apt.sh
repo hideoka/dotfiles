0405e225b64059ef019cd0d5c98df7a302288b3e
Hideaki Nishioka <hideaki.nishioka@gmail.com>
2018-12-24 13:00:29 +0900

    Fixed package name

--- etc/apt.sh
+++ etc/apt.sh
@@ -30,7 +30,7 @@ sudo apt -y install build-essential\
                     tmux\
                     autoconf\
                     pkg-config\
-                    nniversal-ctags
+                    universal-ctags
 
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb
 sudo dpkg -i ripgrep_0.10.0_amd64.deb
