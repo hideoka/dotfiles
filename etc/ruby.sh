b4b79d4afa8fc40190b7b2b53b9e760daf544322
Hideaki Nishioka <hideaki.nishioka@gmail.com>
2018-12-24 19:10:59 +0900

    Updated README and python and ruby installation

--- etc/ruby.sh
+++ etc/ruby.sh
@@ -6,9 +6,8 @@ export LANG=C
 
 cd ./lib/ruby
 
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-source "$HOME/.bash_profile"
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
 
 rbenv install 2.5.3
 rbenv global 2.5.3
