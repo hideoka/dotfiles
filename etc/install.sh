95eb62a1797a2705e7f47444814281cd576ca8dd
Hideaki Nishioka <hideaki@hideoka.dev>
2018-12-29 13:49:05 +0900

    Modified source path

--- etc/install.sh
+++ etc/install.sh
@@ -4,10 +4,10 @@ set -Ceuo pipefail
 if [[ $(uname) == 'Darwin' ]]; then
   echo 'no_install'
 elif [[ $(uname) == 'Linux' ]]; then
-  source ./etc/apt.sh
+  source $DOTFILE_PATH/etc/apt.sh
 else
   echo 'no_install'
 fi
 
-source ./etc/python.sh $DOTFILE_PATH
-source ./etc/ruby.sh $DOTFILE_PATH
+source $DOTFILE_PATH/etc/python.sh $DOTFILE_PATH
+source $DOTFILE_PATH/etc/ruby.sh $DOTFILE_PATH
