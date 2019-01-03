1d680041ee1fec2b5a0802cc4a37f284b3d17d1e
Hideaki Nishioka <hideaki@hideoka.dev>
2019-01-03 21:43:27 +0900

    Modified docker format to rails project

---
+++ lib/docker/etc/setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -Ceuo pipefail
+
+COMMAND="$1"
+
+function check_sync() {
+  local container_name="$(docker container ls |
+    awk -F '  +' '/'"${APP_NAME}-web-sync"'/ NR>1 {print $7}')"
+
+  if [[ -z $container_name ]]; then
+    docker-sync start
+  fi
+}
+
+function change_database_config_file() {
+  cat $ROOT_PATH/config/database.yml |
+    (rm $ROOT_PATH/config/database.yml;\
+      sed -e "1,30s/password:/password: <%= ENV['MYSQL_ROOT_PASSWORD'] %>/"\
+          -e "1,30s/host: localhost/host: db/" > $ROOT_PATH/config/database.yml)
+}
+
+if [[ $COMMAND == 'sync_check' ]]; then
+  check_sync
+elif [[ $COMMAND == 'generator' ]]; then
+  source $ROOT_PATH/etc/generator.sh
+elif [[ $COMMAND == 'db_config' ]]; then
+  change_database_config_file
+fi
