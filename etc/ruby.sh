6327f5d07d053479e6b869bdf3340f1f3ee7ba0a
Hideaki Nishioka <hideaki@hideoka.dev>
2019-01-14 11:19:59 +0900

    Modified shell script

--- etc/ruby.sh
+++ etc/ruby.sh
@@ -1,11 +1,9 @@
 DOTFILE_PATH=$1
 cd $DOTFILE_PATH/lib/ruby
 
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+rbenv init
 
 rbenv install 2.5.3
 rbenv global 2.5.3
-rbenv rehash
-gem install bundler
+gem install bundler -v 1.17
 bundle install
