5b749710e4a87d8506d1eb9940b84e3fc348e16c
Hideaki Nishioka <hideaki@hideoka.dev>
2019-01-27 11:11:04 +0900

    Modified linux brew config for apt.sh

--- etc/apt.sh
+++ etc/apt.sh
@@ -13,5 +13,7 @@ sudo update-locale
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 MANPATH=''
 INFOPATH=''
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
 echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
