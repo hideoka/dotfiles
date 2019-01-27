21ef6d7e8c90eb719228d30b05ea07f69c7de4d7
Hideaki Nishioka <hideaki.nishioka@gmail.com>
2019-01-28 06:29:05 +0900

    Modified installation condition of linuxbrew

--- etc/apt.sh
+++ etc/apt.sh
@@ -10,10 +10,12 @@ sudo apt -y install build-essential\
 
 sudo locale-gen "en_US.UTF-8"
 sudo update-locale
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-MANPATH=''
-INFOPATH=''
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+if ! which brew > /dev/null 2>&1; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+  MANPATH=''
+  INFOPATH=''
+  test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+  test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+  test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+  echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+fi
