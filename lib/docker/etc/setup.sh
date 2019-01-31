e7a8048de5a53f7d0706cbc83702b0589a2eee2c
Hideaki Nishioka <hideaki.nishioka@gmail.com>
2019-01-31 19:27:59 +0900

    Updated_docker_file

--- lib/docker/etc/setup.sh
+++ lib/docker/etc/setup.sh
@@ -4,6 +4,7 @@ set -Ceuo pipefail
 COMMAND="$1"
 
 function check_sync() {
+  ! $DOCKER_SYNC_CHECK  && return;
   local container_name="$(docker container ls |
     awk -F '  +' '/'"${APP_NAME}-web-sync"'/ NR>1 {print $7}')"
 
@@ -12,19 +13,58 @@ function check_sync() {
   fi
 }
 
+function docker_compose_command() {
+  if $DOCKER_SYNC_CHECK; then
+    DOCKER_COMPOSE="docker-compose -f docker-compose.yml -f docker-compose-dev.yml"
+  else
+    DOCKER_COMPOSE="docker-compose -f docker-compose.yml"
+  fi
+}
+
+function docker_compose_rails_new() {
+  eval "${DOCKER_COMPOSE} run --rm web\
+    rails new . -fT -d mysql --webpack\
+    --skip-sprockets --skip-turbolinks --skip-coffee "
+}
+
 function change_database_config_file() {
-  cat $ROOT_PATH/config/database.yml |
-    (rm $ROOT_PATH/config/database.yml;\
-      sed -e "1,30s/password:/password: <%= ENV['MYSQL_ROOT_PASSWORD'] %>/"\
-          -e "1,30s/host: localhost/host: db/" > $ROOT_PATH/config/database.yml)
+  sed -i '' -e "1,30s/password:/password: <%= ENV['MYSQL_ROOT_PASSWORD'] %>/"\
+            -e "1,30s/host: localhost/host: db/"\
+            $ROOT_PATH/config/database.yml
+}
+
+function docker_compose_db_init() {
+  eval "${DOCKER_COMPOSE} run --rm web rails db:create"
+  eval "${DOCKER_COMPOSE} run --rm web rails db:migrate"
+}
+
+function docker_compose_build() {
+  eval "${DOCKER_COMPOSE} build"
+}
+
+function docker_compose_up() {
+  eval "${DOCKER_COMPOSE} up -d"
 }
 
 if [[ $COMMAND == 'sync_check' ]]; then
-  if $DOCKER_SYNC; then
-    check_sync
-  fi
+  check_sync
 elif [[ $COMMAND == 'generator' ]]; then
   source $ROOT_PATH/etc/generator.sh
-elif [[ $COMMAND == 'db_config' ]]; then
+elif [[ $COMMAND == 'init' ]]; then
+  docker_compose_command
+  docker_compose_rails_new
+  docker_compose_build
   change_database_config_file
+  docker_compose_db_init
+elif [[ $COMMAND == 'app_init' ]]; then
+  docker_compose_command
+  docker_compose_build
+  sleep 60
+  docker_compose_db_init
+elif [[ $COMMAND == 'build' ]]; then
+  docker_compose_command
+  docker_compose_build
+elif [[ $COMMAND == 'up' ]]; then
+  docker_compose_command
+  docker_compose_up
 fi
