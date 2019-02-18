29712212881d67b12212c0768bacc61da81463fb
Hideaki Nishioka <hideaki@hideoka.dev>
2019-02-18 22:37:22 +0900

    Added and Changed brew command

--- etc/brew.sh
+++ etc/brew.sh
@@ -5,10 +5,16 @@ brew install pyenv
 brew install rbenv
 brew install tmux
 brew install ripgrep
-brew install --without-doc --HEAD universal-ctags/universal-ctags/universal-ctags
+brew tap universal-ctags/universal-etags
+brew install --HEAD universal-ctags
 brew install tig
-brew install node
+brew install yarn
 brew install pipenv
 brew install gnu-sed
 brew install bat
 brew install httpie
+brew install gawk
+brew tap sanemat/font
+brew install ricty
+cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
+fc-cache -vf
--- etc/osx.sh
+++ etc/osx.sh
@@ -1,1 +1,3 @@
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! which brew > /dev/null 2>&1; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
