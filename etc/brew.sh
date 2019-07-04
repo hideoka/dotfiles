ce3f7308f46a4a71d6eab8363d1914c07d38d6d9
Hideaki Nishioka <hideaki.nishioka@gmail.com>
2019-07-04 23:00:50 +0900

    Modified brew list and condition

--- etc/brew.sh
+++ etc/brew.sh
@@ -1,25 +1,27 @@
 brew install zsh
-brew install gawk
 brew install neovim
 brew install pyenv
 brew install rbenv
 brew install tmux
 brew install ripgrep
 brew tap universal-ctags/universal-ctags
-brew install --HEAD universal-ctags
+brew install --HEAD universal-ctags --without-doc
 brew install tig
 brew install yarn
 brew install pipenv
 brew install gnu-sed
 brew install bat
 brew install httpie
-brew install gawk
-brew tap sanemat/font
-brew install ricty
-cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
-fc-cache -vf
-brew install unison
-brew install eugenmayer/dockersync/unox
 brew install postgresql
 brew install pgcli
-brew install reattach-to-user-namespace
+
+if [[ $(uname) == 'Darwin' ]]; then
+  brew install gawk
+  brew tap sanemat/font
+  brew install ricty
+  cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
+  fc-cache -vf
+  brew install reattach-to-user-namespace
+  brew install unison
+  brew install eugenmayer/dockersync/unox
+fi
