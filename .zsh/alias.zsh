494ea6257c38f410109cc4e16ee1cf0028fa82df
Hideaki Nishioka <hideaki.nishioka@gmail.com>
2019-08-24 10:22:06 +0900

    Modify alias configure

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -3,13 +3,12 @@ alias mkcd="mkdir_and_cd"
 
 alias gd="git diff"
 alias gs="git status"
+alias gl="git log"
+alias glp="git log -p"
 alias gco="git_checkout"
 alias ga="git add"
 alias gc="git commit"
 alias grh="git reset head"
-
-alias dcu="docker-compose up -d"
-alias dcd="docker-compose down"
 
 alias vi="nvim"
 
