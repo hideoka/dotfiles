b645f0c7f8d5463cfc996f90dfdede14a95c884e
Hideaki Nishioka <hideaki.nishioka@gmail.com>
2020-04-05 21:51:16 +0900

    remove zplug

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -3,9 +3,6 @@ zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-autosuggestions"
-# zplug "mafredri/zsh-async", from:github
-# zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
-# zplug "b4b4r07/enhancd", use:init.sh
 zplug "rupa/z", use:"*.sh"
 
 if ! zplug check --verbose; then
