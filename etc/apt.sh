bf4d446db88c47f5d896a8d028b729f09ff6187e
Hideaki Nishioka <hideaki.nishioka@gmail.com>
2020-10-04 09:11:30 +0900

    Remove linuxbrew and update command when linux

--- etc/apt.sh
+++ etc/apt.sh
@@ -1,22 +1,12 @@
 sudo apt -y update
-sudo apt -y install build-essential\
-                    curl\
-                    file\
-                    zlib1g-dev\
-                    libffi-dev\
-                    libreadline-dev\
-                    libssl-dev\
+sudo apt -y install libpq-dev\
+                    libmysqlclient-dev\
+                    libsqlite3-dev\
+                    llvm-dev\
+                    libclang-dev\
+                    chang\
                     zsh\
-                    locales
-
-sudo locale-gen "en_US.UTF-8"
-sudo update-locale
-if ! which brew > /dev/null 2>&1; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-  MANPATH=''
-  INFOPATH=''
-  test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-  test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-  test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-  echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
-fi
+                    neovim\
+                    tmux\
+                    tig\
+                    vim
