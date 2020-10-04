279b0c84b63fc9a570d5b25986407a4746fa7b96
Hideaki Nishioka <hideaki@hideoka.dev>
2020-10-04 21:18:11 +0900

    Update to install bat and ripgrep

--- etc/rust.sh
+++ etc/rust.sh
@@ -1,3 +1,6 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 rustup update
 rustup component add rustfmt clippy rls rust-analysis rust-src
+
+cargo install ripgrep
+cargo install --locked bat
