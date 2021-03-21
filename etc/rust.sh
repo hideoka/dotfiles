b3682a93c15b3cf1521b6fb107d65e2c4d39e177
Hideaki Nishioka <hideaki@hideoka.dev>
2021-03-21 11:49:54 +0900

    Update cargo install list

--- etc/rust.sh
+++ etc/rust.sh
@@ -4,3 +4,8 @@ rustup component add rustfmt clippy rls rust-analysis rust-src
 
 cargo install ripgrep
 cargo install --locked bat
+cargo install delta
+cargo install exa
+cargo install rg
+cargo install tokei
+cargo install xsv
