e18b7ab95f9f9c24ae1257b6ab73e9993be5c883
Hideaki Nishioka <hideaki@hideoka.dev>
2021-08-04 20:34:06 +0900

    Modify initial command for pyenv

--- .zshrc
+++ .zshrc
@@ -31,7 +31,7 @@ eval "$(rbenv init -)"
 eval "$(nodenv init -)"
 
 if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
+  eval "$(pyenv init --path)"
 fi
 eval "$(pyenv virtualenv-init -)"
 
--- etc/python.sh
+++ etc/python.sh
@@ -4,7 +4,7 @@ if [[ $(uname) == 'Linux' ]]; then
   git clone https://github.com/pyenv/pyenv.git ~/.pyenv
   echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
   echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-  echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
+  echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init --path)"\nfi' >> ~/.bash_profile
   exec "$SHELL"
 
   git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
@@ -13,7 +13,7 @@ if [[ $(uname) == 'Linux' ]]; then
 fi
 
 if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
+  eval "$(pyenv init --path)"
   eval "$(pyenv virtualenv-init -)"
 fi
 
