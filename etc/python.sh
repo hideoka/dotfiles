f6a3bb8488f367eddd84373d298553897e912474
Hideaki Nishioka <hideaki@hideoka.dev>
2022-05-11 22:24:19 +0900

    Delete pyenv-virtualenv

--- etc/python.sh
+++ etc/python.sh
@@ -6,27 +6,14 @@ if [[ $(uname) == 'Linux' ]]; then
   echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
   echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init --path)"\nfi' >> ~/.bash_profile
   export PATH="$HOME/.pyenv/bin:$PATH"
-
-  git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-  echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
 fi
 
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
-  set +u
-  eval "$(pyenv virtualenv-init -)"
-  set -u
 fi
 
 pyenv install "$PYTHON_VERSION"
 pyenv global "$PYTHON_VERSION"
 
-# pyenv virtualenv "$PYTHON_VERSION" py3nvim
-# pyenv activate py3nvim
-# pip install pynvim
-# pyenv deactivate
-
-if [[ $(uname) == 'Linux' ]]; then
-  pip install pgcli
-fi
+pip install neovim
