f7d1e8ad8b6b6ef008fbf43aa27d91ec18762b95
Hideaki Nishioka <hideaki@hideoka.dev>
2022-05-18 08:04:02 +0900

    Add sort options for git branch

--- .zsh/fzf.zsh
+++ .zsh/fzf.zsh
@@ -3,7 +3,7 @@ export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
 
 git_checkout() {
   local branches branch
-  branches=$(git branch -vv) &&
+  branches=$(git branch -vv --sort=-authordate) &&
   branch=$(echo "$branches" | fzf +m) &&
   git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
 }
