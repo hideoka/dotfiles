0f74e59878c9509d450dc181234d59ceb3ed9027
Hideaki Nishioka <hideaki@hideoka.dev>
2024-03-20 19:55:07 +0900

    Add lua_ls

--- .vim/init.lua
+++ .vim/init.lua
@@ -354,6 +354,30 @@ lspconfig['gopls'].setup {
   }
 }
 
+lspconfig['lua_ls'].setup {
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      return
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        version = 'LuaJIT'
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+        }
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
+}
+
 require"fidget".setup{}
 
 local cmp = require 'cmp'
