1c410cdebd99ced0365889b5bd50ad452f1a77ed
Hideaki Nishioka <hideaki@hideoka.dev>
2024-03-30 10:20:34 +0900

    Add config of nvim-tree

--- .vim/init.lua
+++ .vim/init.lua
@@ -216,7 +216,12 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
+  sort = {
+    sorter = "case_sensitive"
+  },
+  view = {
+    width = 30
+  },
   renderer = {
     group_empty = true,
   },
