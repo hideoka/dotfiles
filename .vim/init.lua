f173344e9a12825bf24a046f4654fb65060a8c10
Hideaki Nishioka <hideaki@hideoka.dev>
2024-03-30 10:20:53 +0900

    Add tab options

--- .vim/init.lua
+++ .vim/init.lua
@@ -508,6 +508,9 @@ vim.opt.expandtab = true
 vim.opt.listchars = { tab = '»-', trail = '-', eol = '$', extends = '»', precedes = '«', nbsp = '%', space = '⋅' }
 vim.opt.list = true
 vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.swapfile = false
 vim.opt.title = true
