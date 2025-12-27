2275bfa6624bc58ba4c4c42def3f555b11b9b96c
Hideaki Nishioka <hideaki@hideoka.dev>
2025-12-27 09:02:48 +0900

    Update rust lib

--- etc/rust.sh
+++ etc/rust.sh
@@ -5,7 +5,12 @@ cargo install ripgrep
 cargo install bat --locked
 cargo install git-delta
 cargo install cargo-edit
+cargo install cargo-update
 cargo install starship --locked
 cargo install sqlx-cli
 cargo install sheldon
+cargo install eza
+cargo install hexyl
 cargo install mise
+cargo install rust-script
+cargo install typos-cli --locked
